3daaa266409e14133f2be3ff24f0c174ef00660c
jeff60907 <jeff60907@gmail.com>
2017-08-01 17:53:27 +0800

    Build soil moisture sensor on arduino

---
+++ soil_moisture-yl69.ino
@@ -0,0 +1,21 @@
+int sensor_pin = A0;
+int output_value ;
+
+void setup() {
+   Serial.begin(9600);
+}
+
+void loop() {
+
+   output_value= analogRead(sensor_pin);
+   output_value =  1023 - output_value;
+   //Serial.print("Mositure:");
+   Serial.print(output_value);
+   output_value = map(output_value,0,800,0,100);
+   Serial.print(",");
+   Serial.print(output_value);
+   //Serial.print("%");
+
+   delay(500);
+
+}
